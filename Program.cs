7d91e2114aeca2575a9c20dbfd7bcb2c1f9d7d16
dahoo <dahoo@DESKTOP-SE8G5DJ>
2025-08-25 16:07:14 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Input #: ");
+int first_num = Convert.ToInt32(Console.ReadLine());
+int second_num = Convert.ToInt32(Console.ReadLine());  
+int output = first_num + second_num;
+Console.WriteLine($"sum of both number is: {output}");
